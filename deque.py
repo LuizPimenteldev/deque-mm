1a2d68c7c6057e5d82ea2510263fcde775e6c9cd
Luiz Eduardo <124412405+LuizPimenteldev@users.noreply.github.com>
2024-03-07 20:28:40 -0300

    deque.py

--- deque.py
+++ deque.py
@@ -1,15 +1,23 @@
 class Deque:
-    def __init__(self):
-        self.items = []
+    def __init__(self):   # definindo a inicialização da classe.
+        self.items = []   # criando a lista items.
 
-    def adicionar_inicio(self, item):
-        self.items.insert(0, item)
+    def adicionar_inicio(self, item):   # adicionando um novo item no início do deque.
+        self.items.insert(0, item)      # o insert insere o item na lista na posição 0 e desloca os elementos pra direita.
 
-    def adicionar_final(self, item):
-        self.items.append(item)
+    def adicionar_final(self, item):   # aqui tem o mesmo propósito, porém no final do deque.
+        self.items.append(item)        # o append fica melhor de usar pois sempre coloca o elemento no final, e o insert é usado pra colocar na posição específica, pra evitar algum erro de alocação.
+                                        # (obs poderia utilizar o insert sem problema, mas preferimos não usar, pra não ter que criar um len pros itens XD)
 
-    def remover_inicio(self):
+    def remover_inicio(self):         # remove o item no inicio 
         return self.items.pop(0)
+
+    def remover_final(self):
+        if self.esta_vazio():
+            raise ValueError("O deque está vazio, não é possível remover elementos.")
+
+        return self.items.pop()
+
 
     def tamanho(self):
         return len(self.items)
